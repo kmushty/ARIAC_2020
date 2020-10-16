b4e03d6335f38c36194767a837fcaed7d122f900
Eashwar-S <eashwar.sathyamurthy@gmail.com>
2020-10-15 22:52:24 -0400

    Uploading not working files

--- ariac_ws/src/rwa3_3/src/rwa3_1.cpp
+++ ariac_ws/src/rwa3_3/src/rwa3_1.cpp
@@ -40,9 +40,82 @@
 
 #include <tf2/LinearMath/Quaternion.h>
 
+class LogicalCameraSubscriberClass{
+public:
+    part my_part1;
+
+    void logical_camera_callback(
+    const nist_gear::LogicalCameraImage::ConstPtr &msg) {
+        if (msg->models.size() > 0) {
+            tf2_ros::Buffer tfBuffer;
+            tf2_ros::TransformListener tfListener(tfBuffer);
+            ros::Duration timeout(5.0);
+            geometry_msgs::TransformStamped T_w_l;
+            geometry_msgs::PoseStamped p_w, p_l;
+
+
+            std::string name = "logical_camera_11_frame";
+
+            try {
+                T_w_l = tfBuffer.lookupTransform("world", name,
+                                                 ros::Time(0), timeout);
+            }
+            catch (tf2::TransformException &ex) {
+                ROS_WARN("%s", ex.what());
+                ros::Duration(1.0).sleep();
+            }
+
+            for (int i = 0; i < msg->models.size(); i++) {
+                if(msg->models[i].type == "pulley_part_red"){
+                    std::string quaSenName;
+                    ROS_INFO("----------------+------------------");
+                    p_l.header.frame_id = "logical_camera_11_frame";
+                    p_l.pose = msg->models[i].pose;
+                    tf2::doTransform(p_l, p_w, T_w_l);
+
+                    //--You should receive the following information from a camera
+
+                    my_part1.type = msg->models[i].type;
+                    my_part1.pose.position.x = p_w.pose.position.x;
+                    my_part1.pose.position.y = p_w.pose.position.y;
+                    my_part1.pose.position.z = p_w.pose.position.z;
+                    my_part1.pose.orientation.x = p_w.pose.orientation.x;
+                    my_part1.pose.orientation.y = p_w.pose.orientation.y;
+                    my_part1.pose.orientation.z = p_w.pose.orientation.z;
+                    my_part1.pose.orientation.w = p_w.pose.orientation.w;
+
+
+                    ROS_INFO("------------------------------");
+                    ROS_INFO("%f", my_part1.pose.position.x);
+                    ROS_INFO("%f", my_part1.pose.position.y);
+                    ROS_INFO("%f", my_part1.pose.position.z);
+                    ROS_INFO("------------------------------");
+
+//                    ROS_INFO("Target Part details collected successfully");
+//                    ROS_INFO("%s in world frame:\n\n"
+//                             "Position: [x,y,z] = [%f,%f,%f]\n"
+//                             "Orientation: [x,y,z,w] = [%f,%f,%f,%f]\n",
+//                             msg->models[i].type.c_str(),
+//                             p_w.pose.position.x,
+//                             p_w.pose.position.y,
+//                             p_w.pose.position.z,
+//                             p_w.pose.orientation.x,
+//                             p_w.pose.orientation.y,
+//                             p_w.pose.orientation.z,
+//                             p_w.pose.orientation.w);
+                    break;
+                }
+            }
+        }
+    }
+};
+
 
 class OrderSubscriberClass {
 public:
+    // create order variable
+    nist_gear::Order::ConstPtr order;
+    part mypart1;
     explicit OrderSubscriberClass(ros::NodeHandle &node){
 
     }
@@ -53,7 +126,11 @@ public:
         ROS_INFO("%s", orderDetails->order_id.c_str());
         ROS_INFO("Information regarding order collected.");
 
+        // set part to order details
+
+
     }
+
 
     nist_gear::Order::ConstPtr returnOrderMessage(){
         return orderDetails;
@@ -85,7 +162,8 @@ int main(int argc, char ** argv) {
     part requiredPart;
     part part_in_tray;
     std::string quaSenName;
-
+    LogicalCameraSubscriberClass logi;
+//    ROS_INFO("%s",orderDetails->order_id.c_str());
     //--1-Read order
     // Pre-kitting -
     //   1. Starting the competition
@@ -98,6 +176,16 @@ int main(int argc, char ** argv) {
     orderDetails = orderObject.returnOrderMessage();
     ROS_INFO("Checkpoint - 2");
 
+
+
+
+
+    ros::Subscriber logicalCameraSub = node.subscribe("/ariac/logical_camera_11", 10, &LogicalCameraSubscriberClass::logical_camera_callback, &logi);
+    ros::spin();
+    ROS_INFO("Checkpoint - 3");
+    ROS_INFO("%f", logi.my_part1.pose.position.x);
+    ROS_INFO("%f", logi.my_part1.pose.position.y);
+    ROS_INFO("%f", logi.my_part1.pose.position.z);
 
     //--1-Read order
     //--2-Look for parts in this order
