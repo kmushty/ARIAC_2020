2f4f09f847c200fa746d09c6ccf8259f5345f7b9
ak <mesney.mesney@gmail.com>
2020-10-16 06:16:00 -0400

    Added and modified relevant header files

---
+++ ariac_ws/src/rwa3_3/include/camera.h
@@ -0,0 +1,52 @@
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <ros/ros.h>
+
+#include <nist_gear/LogicalCameraImage.h>
+#include <nist_gear/Order.h>
+#include <nist_gear/Proximity.h>
+#include <sensor_msgs/LaserScan.h>
+#include <sensor_msgs/Range.h>
+#include <std_msgs/Float32.h>
+#include <std_srvs/Trigger.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h> //--needed for tf2::Matrix3x3
+#include <map>
+#include "boost/bind.hpp"
+
+
+
+const int NUM_LOGICAL_CAMERAS = 17;
+
+class Camera{
+
+  public:
+    void logical_camera_callback(
+    const nist_gear::LogicalCameraImage::ConstPtr &msg, int index);
+
+    void break_beam_callback(const nist_gear::Proximity::ConstPtr &msg);
+
+    void laser_profiler_callback(const sensor_msgs::LaserScan::ConstPtr & msg);
+    
+    void init(ros::NodeHandle & node);
+
+    std::map<std::string,std::vector<geometry_msgs::PoseStamped>> get_detected_parts();
+
+    Camera();
+ 
+
+
+            
+
+  private:
+    ros::Subscriber logical_camera_subscriber[NUM_LOGICAL_CAMERAS];
+    std::map<std::string,std::vector<geometry_msgs::PoseStamped>> detected_parts;
+
+};
+
+
+
+
+
+
