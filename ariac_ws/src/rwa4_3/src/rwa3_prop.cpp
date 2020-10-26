33e36fb91e263bc3b0f9f35d121bbef3d47b86c4
Eashwar-S <eashwar.sathyamurthy@gmail.com>
2020-10-26 01:19:08 -0400

    Implemented Faulty gripper functionality

--- ariac_ws/src/rwa4_3/src/rwa3_prop.cpp
+++ ariac_ws/src/rwa4_3/src/rwa3_prop.cpp
@@ -16,6 +16,7 @@
 #include <algorithm>
 #include <vector>
 #include <ros/ros.h>
+#include <math.h>
 
 #include <nist_gear/LogicalCameraImage.h>
 #include <nist_gear/Order.h>
@@ -64,7 +65,8 @@ void moveToStartLocation(std::map<std::string,std::vector<PresetLocation>> &pres
     auto vec = presetLoc[location];
     if(vec.size() == 1){
         gantry.goToPresetLocation(vec[0]);
-        gantry.goToPresetLocation(gantry.start_);
+        if(location != "start")
+            gantry.goToPresetLocation(gantry.start_);
     }
     else{
         for(int i=vec.size()-2; i>=0;i--){
@@ -139,7 +141,7 @@ int main(int argc, char ** argv) {
     std::string agvId;
     bool foundPart = false;
     int index = 0;
-    part my_part, my_part_in_tray;
+    part my_part, my_part_in_tray, placed_part, actual_part;
     //std::map<std::string,std::vector<part>> detected_parts;
     std::map<std::string,part> detected_parts;
     
@@ -163,69 +165,79 @@ int main(int argc, char ** argv) {
                     for(auto const& parts: detected_parts) {
                         if (parts.first == "logical_camera_8" || parts.first == "logical_camera_10")    // agv cameras
                             continue;
-                        
+
                         index = 0;
-                        if(product.type == parts.second.type.c_str()){
-                              my_part = parts.second;
-                              foundPart = true;
-                           
-                              my_part_in_tray.type = product.type;
-                              my_part_in_tray.pose = product.pose;
+                        if (product.type == parts.second.type.c_str()) {
+                            my_part = parts.second;
+                            foundPart = true;
 
-                              moveToPresetLocation(presetLoc,parts.first,gantry);
+                            my_part_in_tray.type = product.type;
+                            my_part_in_tray.pose = product.pose;
 
-                              gantry.pickPart(my_part);
-                              moveToStartLocation(presetLoc,parts.first,gantry);
+                            moveToPresetLocation(presetLoc, parts.first, gantry);
 
-                              if(int(my_part_in_tray.pose.orientation.x) == 1){
-                                  gantry.goToPresetLocation(gantry.go_to_flipped_pulley_);
-                                  gantry.activateGripper("right_arm");
-                                  gantry.deactivateGripper("left_arm");
-                                  gantry.goToPresetLocation(gantry.agv2_flipped_);
-                                  my_part_in_tray.pose.orientation.x = 0;
-                                  my_part_in_tray.pose.orientation.w = 1;
-                                
-                                  gantry.placeFlippedPart(my_part_in_tray,"agv2","right_arm");
-                              }else
-                                  gantry.placePart(my_part_in_tray, "agv2", "left_arm");
+                            gantry.pickPart(my_part);
+                            moveToStartLocation(presetLoc, parts.first, gantry);
 
-                              moveToStartLocation(presetLoc,"start",gantry);
+                            if (int(my_part_in_tray.pose.orientation.x) == 1) {
+                                gantry.goToPresetLocation(gantry.go_to_flipped_pulley_);
+                                gantry.activateGripper("right_arm");
+                                gantry.deactivateGripper("left_arm");
+                                gantry.goToPresetLocation(gantry.agv2_flipped_);
+                                my_part_in_tray.pose.orientation.x = 0;
+                                my_part_in_tray.pose.orientation.w = 1;
+
+                                gantry.placeFlippedPart(my_part_in_tray, agvId, "right_arm");
+                            } else
+                                gantry.placePart(my_part_in_tray, agvId, "left_arm");
+
+                            moveToStartLocation(presetLoc, agvId, gantry);
+
+                            //TODO Determining preset location for agv1
+                            // Faulty Gripper Implementation
+                            if (agvId == "agv2")
+                                placed_part = detected_parts["logical_camera_10"];
+                            else
+                                placed_part = detected_parts["logical_camera_8"];
+
+                            actual_part.type = placed_part.type;
+                            actual_part.pose = gantry.getTargetWorldPose(my_part_in_tray.pose, agvId, "left_arm");
+                            if(sqrt(pow(actual_part.pose.position.x - placed_part.pose.position.x, 2) +
+                                    pow(actual_part.pose.position.y - placed_part.pose.position.y, 2) +
+                                    pow(actual_part.pose.position.z - placed_part.pose.position.x, 2)) > 0.03) {
+                                ROS_INFO("Faulty Gripper Detected");
+                                ROS_INFO("Re-picking and replacing the part");
+                                moveToPresetLocation(presetLoc, agvId, gantry);
+                                gantry.pickPart(placed_part);
+                                gantry.placePart(actual_part, agvId, "left_arm");
+                            }
 
 
-                              ros::spinOnce();
-                              ros::spinOnce();
-                              if(camera.get_is_faulty()) {
-                                  ROS_INFO_STREAM("removing faulty parts");
-                                  
-                                  part temp;
+                            ros::spinOnce();
+                            ros::spinOnce();
+                            if (camera.get_is_faulty()) {
+                                ROS_INFO_STREAM("removing faulty parts");
 
-                                  temp.pose = camera.get_faulty_pose();
-                                 // temp.type = "pulley_part_red";
-                                  temp.type = my_part.type;
-                                  ROS_INFO_STREAM(temp.pose);
+                                part temp;
 
-                                  moveToPresetLocation(presetLoc,"agv2",gantry);
-                                  ROS_INFO_STREAM("no nononononononoons");
-                                  gantry.pickPart(temp);
-                                  ROS_INFO_STREAM("Hahahahahahaha");
-                                  moveToStartLocation(presetLoc,"start",gantry);
+                                temp.pose = camera.get_faulty_pose();
+                                // temp.type = "pulley_part_red";
+                                temp.type = my_part.type;
+                                ROS_INFO_STREAM(temp.pose);
 
-                                  //gantry.placePart(temp);
-                                  //gantry.dropPart(temp);
-                                  //gantry.presetArmLocation(gantry.start_);
-                                  foundPart = false;
-                                  camera.reset_is_faulty();
-                                  gantry.deactivateGripper("left_arm");
-                              }
+                                moveToPresetLocation(presetLoc, agvId, gantry);
+                                ROS_INFO_STREAM("no nononononononoons");
+                                gantry.pickPart(temp);
+                                ROS_INFO_STREAM("Hahahahahahaha");
+                                moveToStartLocation(presetLoc, agvId, gantry);
+                                foundPart = false;
+                                camera.reset_is_faulty();
+                                gantry.deactivateGripper("left_arm");
+                            }
 
-                              //camera.remove_part(parts.first, index);
-                              //index++;
-
-                              break;
-                          }
-
+                            break;
                         }
-
+                    }
                     ros::spinOnce();
                 }
 
