6c7e3b4f784789b4a0847dfbc6da21723e3c2d38
ak <mesney.mesney@gmail.com>
2020-12-02 00:29:09 -0500

    Rectified wait and movetime error

--- ariac_ws/src/rwa5_3/src/rwa5_prop.cpp
+++ ariac_ws/src/rwa5_3/src/rwa5_prop.cpp
@@ -829,7 +829,7 @@ void estimateObstacleAttributes(Camera &camera,int aisle_num) {
     auto aisle_breakbeam_msgs  = camera.get_aisle_breakbeam_msgs();
 
     auto vec = aisle_breakbeam_msgs[aisle_num];
-    if(obstacleInAisle[aisle_num] == true  &&  obstacleAssociatedWithAisle[aisle_num].is_valid_obstacle == false && vec.size() > 8) {
+    if(obstacleInAisle[aisle_num] == true  &&  obstacleAssociatedWithAisle[aisle_num].is_valid_obstacle == false && vec.size() > 6) {
        std::string str1 =  "shelf_breakbeam_" + std::to_string(aisle_num*3 + 2) + "_frame";
        std::string str2 =  "shelf_breakbeam_" + std::to_string(aisle_num*3 + 1) + "_frame";
 
@@ -879,7 +879,7 @@ void estimateObstacleAttributes(Camera &camera,int aisle_num) {
        ROS_INFO_STREAM(sec3 - sec2);
 
        double velocity = LENGTH_OF_AISLE/move_time;
-       double minTriggerDistanceBtnLastBB   = 3.715;
+       double minTriggerDistanceBtnLastBB   = 3.4;
 
        obstacle human;
        human.wait_time = round(wait_time);
